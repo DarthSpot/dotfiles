6b60c9455355124b60b1eca4e61ed7a5db3b5e67
Alexander Mills <spot.enterprise@web.de>
2022-12-06 18:17:50 +0100

    removed redundancy

--- hypr/hypr/scripts/launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export SDL_VIDEODRIVER=wayland
-export MOZ_ENABLE_WAYLAND=1
-export GTK_THEME=gruvbox-dark-gtk
-export XDG_SESSION_DESKTOP=Hyprland
-export XDG_SESSION_TYPE=wayland
-export XDG_CURRENT_DESKTOP=Hyprland
-exec Hyprland
--- hypr/hypr/scripts/wallpaper.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-swaybg -i ~/Bilder/Wallpaper/hatsune_miku.jpg -m fit -c '#0A101E' 
